0febd30a74bcd40fa11018ee2ef77f99e1542a41
abosch <arturbosch@gmx.de>
2016-12-13 11:44:12 +0100

    Refactor detektTool

--- quide-detekt-implementation/src/main/kotlin/io/gitlab/arturbosch/quide/detekt/DetektTool.kt
+++ quide-detekt-implementation/src/main/kotlin/io/gitlab/arturbosch/quide/detekt/DetektTool.kt
@@ -1,23 +1,23 @@
 package io.gitlab.arturbosch.quide.detekt
 
 import io.gitlab.arturbosch.detekt.api.CodeSmell
-import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.quide.detection.Detector
+import io.gitlab.arturbosch.quide.model.BaseCodeSmell
 import io.gitlab.arturbosch.quide.model.SmellContainer
-import io.gitlab.arturbosch.quide.vcs.Versionable
+import io.gitlab.arturbosch.quide.platform.UserData
 
 /**
  * @author Artur Bosch
  */
-class DetektTool : Detector {
+class DetektTool : Detector<DetektCodeSmell> {
 
-	override fun call(): SmellContainer<DetektCodeSmell> {
+	override fun run(userData: UserData): SmellContainer<DetektCodeSmell> {
 		return object : SmellContainer<DetektCodeSmell> {
-			override fun all(): List<DetektCodeSmell> {
+			override fun all(): MutableList<DetektCodeSmell> {
 				throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
 			}
 
-			override fun findBySourcePath(path: String): List<DetektCodeSmell> {
+			override fun findBySourcePath(path: String?): MutableList<DetektCodeSmell> {
 				throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
 			}
 
@@ -26,41 +26,4 @@ class DetektTool : Detector {
 
 }
 
-class DetektCodeSmell(id: String, entity: Entity) : CodeSmell(id, entity), io.gitlab.arturbosch.quide.model.CodeSmell {
-	override fun startVersion(): Versionable {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-	override fun setStartVersion(versionable: Versionable?) {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-	override fun endVersion(): Versionable {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-	override fun setEndVersion(versionable: Versionable?) {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-	override fun isConsistent(): Boolean {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-	override fun setConsistent(consistent: Boolean) {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-	override fun weight(): Int {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-	override fun addWeight(amount: Int) {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-	override fun sourcePath(): String {
-		throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-	}
-
-}
+class DetektCodeSmell(private val smell: CodeSmell) : BaseCodeSmell()
