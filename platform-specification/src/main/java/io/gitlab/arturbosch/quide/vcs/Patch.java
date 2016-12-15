c326bd56a3482311378813af61afa756ff17e6c7
abosch <arturbosch@gmx.de>
2016-12-15 16:02:37 +0100

    There is no more a platform-specification only quide-specification

--- platform-specification/src/main/java/io/gitlab/arturbosch/quide/vcs/Patch.java
+++
@@ -1,7 +0,0 @@
-package io.gitlab.arturbosch.quide.vcs;
-
-/**
- * @author Artur Bosch
- */
-public interface Patch {
-}
--- platform-specification/src/main/java/io/gitlab/arturbosch/quide/vcs/VersionIterator.java
+++
@@ -1,9 +0,0 @@
-package io.gitlab.arturbosch.quide.vcs;
-
-import java.util.Iterator;
-
-/**
- * @author Artur Bosch
- */
-public interface VersionIterator extends Iterator<Versionable> {
-}
