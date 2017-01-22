0a41ac7982bdc266dbc3f5091669a943ebacb809
abosch <arturbosch@gmx.de>
2017-01-22 14:19:07 +0100

    Print smartsmells result xml file if no version provider provided

--- quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/SmartSmellsTool.kt
+++ quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/SmartSmellsTool.kt
@@ -3,7 +3,11 @@ package io.gitlab.arturbosch.quide.java
 import io.gitlab.arturbosch.quide.detection.Detector
 import io.gitlab.arturbosch.quide.platform.UserData
 import io.gitlab.arturbosch.smartsmells.api.DetectorFacade
+import io.gitlab.arturbosch.smartsmells.api.SmellResult
 import io.gitlab.arturbosch.smartsmells.config.DetectorConfig
+import io.gitlab.arturbosch.smartsmells.out.XMLWriter
+import java.nio.file.Files
+import java.time.LocalDateTime
 
 /**
  * @author Artur Bosch
@@ -15,10 +19,23 @@ class SmartSmellsTool : Detector<JavaSmellContainer> {
 	}
 
 	override fun <U : UserData> execute(data: U): JavaSmellContainer {
-		val projectPath = data.projectPath()
-		val configPath = data.quideDirectory().configurationsDir().resolve("smartsmells.yml")
-		val facade = DetectorFacade.fromConfig(DetectorConfig.load(configPath))
-		val smellResult = facade.run(projectPath)
-		return JavaSmellContainer(smellResult)
+		val currentVersion = data.currentVersion()
+		val lastVersion = data.lastVersion()
+		if (!lastVersion.isPresent) {
+			val configPath = data.quideDirectory().configurationsDir().resolve("smartsmells.yml")
+			val facade = DetectorFacade.fromConfig(DetectorConfig.load(configPath))
+			val projectPath = data.projectPath()
+			val smellResult = facade.run(projectPath)
+
+			if (!currentVersion.isPresent && !lastVersion.isPresent) {
+				val outputPath = data.outputPath().orElse(data.projectPath())
+				val xml = XMLWriter.toXml(smellResult)
+				Files.write(outputPath.resolve("smartsmells_${LocalDateTime.now()}.xml"), xml.toByteArray())
+			}
+
+			return JavaSmellContainer(smellResult)
+		}
+		return JavaSmellContainer(SmellResult())
 	}
+
 }
