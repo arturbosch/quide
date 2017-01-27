7159a335c0368cb7847dc42c595572b029fe56c9
abosch <arturbosch@gmx.de>
2017-01-27 18:18:31 +0100

    Add more default filtering methods for smell container

--- quide-specification/src/main/java/io/gitlab/arturbosch/quide/model/SmellContainer.java
+++ quide-specification/src/main/java/io/gitlab/arturbosch/quide/model/SmellContainer.java
@@ -1,6 +1,7 @@
 package io.gitlab.arturbosch.quide.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Represents a container for smells.
@@ -18,13 +19,42 @@ public interface SmellContainer<T extends CodeSmell> {
 	 * @param path the path used as a filter
 	 * @return a subset of smells for given path
 	 */
-	List<T> findBySourcePath(String path);
+	default List<T> findBySourcePath(String path) {
+		return all().stream()
+				.filter(smell -> smell.sourcePath().equals(path))
+				.collect(Collectors.toList());
+	}
 
 	/**
 	 * Size of the smell container eg. #smells.
+	 *
 	 * @return amount of smells
 	 */
 	default int size() {
 		return all().size();
+	}
+
+	/**
+	 * Filters the container for living smells/not killed within an evolutionary analysis.
+	 *
+	 * @return living smells
+	 */
+	default List<T> alive() {
+		return all().stream()
+				.filter(CodeSmell::isAlive)
+				.collect(Collectors.toList());
+	}
+
+	/**
+	 * Combination of {@link #findBySourcePath(String)} and {@link #alive()}
+	 *
+	 * @param path path smells need to have
+	 * @return smells in given path and which are alive
+	 */
+	default List<T> aliveInPath(String path) {
+		return all().stream()
+				.filter(smell -> smell.sourcePath().equals(path))
+				.filter(CodeSmell::isAlive)
+				.collect(Collectors.toList());
 	}
 }
