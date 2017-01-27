9b77255599588b0e07266d107fd0e02bac8cd9c1
abosch <arturbosch@gmx.de>
2017-01-27 13:46:54 +0100

    Merge VersionCrawler functionality from evolutionary plugin into standard java plugin

--- quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/SmartSmellsTool.kt
+++ quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/SmartSmellsTool.kt
@@ -2,12 +2,15 @@ package io.gitlab.arturbosch.quide.java
 
 import io.gitlab.arturbosch.quide.detection.Detector
 import io.gitlab.arturbosch.quide.platform.UserData
+import io.gitlab.arturbosch.quide.vcs.Versionable
 import io.gitlab.arturbosch.smartsmells.api.DetectorFacade
 import io.gitlab.arturbosch.smartsmells.api.SmellResult
+import io.gitlab.arturbosch.smartsmells.api.UpdatableDetectorFacade
 import io.gitlab.arturbosch.smartsmells.config.DetectorConfig
 import io.gitlab.arturbosch.smartsmells.out.XMLWriter
 import java.nio.file.Files
 import java.time.LocalDateTime
+import java.util.Optional
 
 /**
  * @author Artur Bosch
@@ -21,21 +24,43 @@ class SmartSmellsTool : Detector<JavaSmellContainer> {
 	override fun <U : UserData> execute(data: U): JavaSmellContainer {
 		val currentVersion = data.currentVersion()
 		val lastVersion = data.lastVersion()
-		if (!lastVersion.isPresent) {
-			val configPath = data.quideDirectory().configurationsDir().resolve("smartsmells.yml")
-			val facade = DetectorFacade.fromConfig(DetectorConfig.load(configPath))
-			val projectPath = data.projectPath()
-			val smellResult = facade.run(projectPath)
-
-			if (!currentVersion.isPresent && !lastVersion.isPresent) {
-				val outputPath = data.outputPath().orElse(data.projectPath())
-				val xml = XMLWriter.toXml(smellResult)
-				Files.write(outputPath.resolve("smartsmells_${LocalDateTime.now()}.xml"), xml.toByteArray())
-			}
-
-			return JavaSmellContainer(smellResult)
+		val smellResult = if (!lastVersion.isPresent) {
+			firstVersion(data)
+		} else {
+			nextVersion(currentVersion, data)
 		}
-		return JavaSmellContainer(SmellResult())
+		if (isReportWanted(currentVersion, lastVersion)) {
+			generateReport(data, smellResult)
+		}
+		return JavaSmellContainer(smellResult)
+	}
+
+	private fun <U : UserData> generateReport(data: U, smellResult: SmellResult) {
+		val outputPath = data.outputPath().orElse(data.projectPath())
+		val xml = XMLWriter.toXml(smellResult)
+		Files.write(outputPath.resolve("smartsmells_${LocalDateTime.now()}.xml"), xml.toByteArray())
+	}
+
+	private fun isReportWanted(currentVersion: Optional<Versionable>, lastVersion: Optional<Versionable>) = !currentVersion.isPresent && !lastVersion.isPresent
+
+	private fun <U : UserData> firstVersion(data: U): SmellResult {
+
+		val configPath = data.quideDirectory().configurationsDir().resolve("smartsmells.yml")
+		val facade = DetectorFacade.fromConfig(DetectorConfig.load(configPath))
+		val projectPath = data.projectPath()
+		return facade.run(projectPath)
+	}
+
+	private fun <U : UserData> nextVersion(currentVersion: Optional<Versionable>, data: U): SmellResult {
+		assert(currentVersion.isPresent)
+
+		val projectPath = data.projectPath()
+		val configPath = data.quideDirectory().configurationsDir().resolve("smartsmells.yml")
+
+		val facade = DetectorFacade.fromConfig(DetectorConfig.load(configPath))
+		val updatableDetectorFacade = UpdatableDetectorFacade(projectPath, facade)
+
+		return VersionCrawler(projectPath, currentVersion.get(), updatableDetectorFacade).run()
 	}
 
 }
---
+++ quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/VersionCrawler.kt
@@ -0,0 +1,63 @@
+package io.gitlab.arturbosch.quide.java
+
+import com.github.javaparser.utils.Pair
+import io.gitlab.arturbosch.quide.vcs.FileChange
+import io.gitlab.arturbosch.quide.vcs.Versionable
+import io.gitlab.arturbosch.smartsmells.api.SmellResult
+import io.gitlab.arturbosch.smartsmells.api.UpdatableDetectorFacade
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.nio.file.Path
+
+/**
+ * @author Artur Bosch
+ */
+class VersionCrawler(val projectPath: Path, val versionable: Versionable,
+					 private val facade: UpdatableDetectorFacade) {
+
+	private val logger: Logger = LoggerFactory.getLogger(javaClass.simpleName)
+
+	fun run(): SmellResult {
+		return generateNewCompilationUnits(versionable).apply {
+			logger.info("Successful executed detectors with ${smellSets.map { "${it.key} - ${it.value.size} " }}")
+		}
+	}
+
+	private fun generateNewCompilationUnits(current: Versionable): SmellResult {
+
+		val sequence = current.fileChanges().asSequence()
+		val additions = sequence.filter { it.isOfType(FileChange.Type.ADDITION) }
+				.map { it.newFile() }.map { projectPath.resolve(it.path()) to it.content() }
+				.filter { it.first.toString().endsWith(".java") }
+				.filter { !it.second.isNullOrEmpty() }
+				.toMap()
+		val modifications = sequence.filter { it.isOfType(FileChange.Type.MODIFICATION) }
+				.map { it.newFile() }.map { projectPath.resolve(it.path()) to it.content() }
+				.filter { it.first.toString().endsWith(".java") }
+				.filter { !it.second.isNullOrEmpty() }
+				.toMap()
+		val relocations = sequence.filter { it.isOfType(FileChange.Type.RELOCATION) }
+				.map { change ->
+					projectPath.resolve(change.oldFile().path()) to
+							Pair(projectPath.resolve(change.newFile().path()), change.newFile().content())
+				}
+				.filter { it.first.toString().endsWith(".java") }
+				.filter { it.second.a.toString().endsWith(".java") }
+				.filter { !it.second.b.isNullOrEmpty() }
+				.toMap()
+		val deletions = sequence.filter { it.isOfType(FileChange.Type.REMOVAL) }
+				.map { it.oldFile() }
+				.map { projectPath.resolve(it.path()) }
+				.filter { it.toString().endsWith(".java") }
+				.toList()
+		logger.info("Parsed all changes...")
+		val size = additions.size + modifications.size + relocations.size
+		logger.info("Running detectors with $size compilation info's.")
+		facade.remove(deletions)
+		facade.addOrUpdate(additions)
+		facade.addOrUpdate(modifications)
+		facade.relocateWithContent(relocations)
+		return facade.run()
+	}
+
+}
