abcf99830a2bcbf84dc43d34cf575457996c7573
abosch <arturbosch@gmx.de>
2017-02-07 18:48:48 +0100

    Introduce searchForField method to reduce code duplication for FieldSpecific smells

--- quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/mapping/ASTPatch.kt
+++ quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/mapping/ASTPatch.kt
@@ -3,8 +3,10 @@ package io.gitlab.arturbosch.quide.java.mapping
 import com.github.javaparser.ast.CompilationUnit
 import com.github.javaparser.ast.Node
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
+import com.github.javaparser.ast.body.FieldDeclaration
 import com.github.javaparser.ast.expr.Expression
 import com.github.javaparser.ast.stmt.Statement
+import difflib.Delta
 import io.gitlab.arturbosch.jpal.ast.ClassHelper
 import io.gitlab.arturbosch.jpal.internal.Printer
 import io.gitlab.arturbosch.quide.java.JavaCodeSmell
@@ -15,6 +17,8 @@ import io.gitlab.arturbosch.smartsmells.smells.ElementTarget
 import io.gitlab.arturbosch.smartsmells.smells.FieldSpecific
 import io.gitlab.arturbosch.smartsmells.smells.LocalSpecific
 import io.gitlab.arturbosch.smartsmells.smells.MethodSpecific
+import io.gitlab.arturbosch.smartsmells.smells.cycle.Cycle
+import io.gitlab.arturbosch.smartsmells.util.Strings
 
 /**
  * @author Artur Bosch
@@ -41,17 +45,20 @@ class ASTPatch(val chunks: List<ASTChunk>, unit: CompilationUnit) : Patch<JavaCo
 		val smell = this.smell
 		if (smell is LocalSpecific) {
 			chunks.asSequence().map { it.anyNodeBySignature(smell.signature()) }
-					.find { it != null }
+					.filterNotNull()
+					.map { it to Strings.distance(it.toSignature(), smell.signature()) }
+					.minBy { it.second }
 					?.let {
-						when (it) {
+						val node = it.first
+						when (node) {
 							is Expression -> {
 								println("TREFFER!")
-								val copy = smell.copy(it)
+								val copy = smell.copy(node)
 								return this.updateInternal(copy)
 							}
 							is Statement -> {
 								println("TREFFER!")
-								val copy = smell.copy(it)
+								val copy = smell.copy(node)
 								return this.updateInternal(copy)
 							}
 							else -> return@let
@@ -73,19 +80,29 @@ class ASTPatch(val chunks: List<ASTChunk>, unit: CompilationUnit) : Patch<JavaCo
 				val copy = smell.copyOnSecond(it)
 				return this.updateInternal(copy)
 			}
-			chunks.asSequence().map { it.nodeByFieldSignature(smell.signature()) }
-					.find { it != null }
-					?.let {
-						println("TREFFER!")
-						val copy = smell.copy(it)
-						return this.updateInternal(copy)
-					} ?: chunks.asSequence().map { it.nodeByFieldSignature(smell.secondSignature()) }
-					.find { it != null }
-					?.let {
-						println("TREFFER!")
-						val copy = smell.copyOnSecond(it)
-						return this.updateInternal(copy)
-					}
+			if (smell is Cycle) {
+				smell.source.searchForField()?.let {
+					println("TREFFER!")
+					val copy = smell.copy(it)
+					return this.updateInternal(copy) }
+				smell.target.searchForField()?.let {
+					println("TREFFER!")
+					val copy = smell.copyOnSecond(it)
+					return this.updateInternal(copy) }
+			}
+//			chunks.asSequence().map { it.nodeByFieldSignature(smell.signature()) }
+//					.find { it != null }
+//					?.let {
+//						println("TREFFER!")
+//						val copy = smell.copy(it)
+//						return this.updateInternal(copy)
+//					} ?: chunks.asSequence().map { it.nodeByFieldSignature(smell.secondSignature()) }
+//					.find { it != null }
+//					?.let {
+//						println("TREFFER!")
+//						val copy = smell.copyOnSecond(it)
+//						return this.updateInternal(copy)
+//					}
 		}
 		return this
 	}
@@ -98,15 +115,24 @@ class ASTPatch(val chunks: List<ASTChunk>, unit: CompilationUnit) : Patch<JavaCo
 				val copy = smell.copy(it)
 				return this.updateInternal(copy)
 			}
-			chunks.asSequence().map { it.nodeByFieldSignature(smell.signature()) }
-					.find { it != null }
-					?.let {
-						println("TREFFER!")
-						val copy = smell.copy(it)
-						return this.updateInternal(copy)
-					}
+			smell.searchForField()?.let { this.updateInternal(smell.copy(it)) }
 		}
 		return this
+	}
+
+	private fun FieldSpecific.searchForField(): FieldDeclaration? {
+		val directMappedMatches = chunks.map { it.nodeByFieldSignature(signature()) }
+				.filterNotNull()
+		// we need to consider insertions as a text diff is not able to provide exact information
+		// about moved+renamed code. Text diff will tell us about a change AND an addition
+		val matchingInserts = chunks.filter { it.type == Delta.TYPE.INSERT }
+				.map { it.revisedNodes.filter { it is FieldDeclaration } }
+				.flatMap { it }
+		return matchingInserts.plus(directMappedMatches)
+				.filterIsInstance(FieldDeclaration::class.java)
+				.map { it to Strings.distance(it.toSignature(), signature()) }
+				.minBy { it.second }
+				?.first
 	}
 
 	private fun JavaCodeSmell.patchMethodLevel(): JavaCodeSmell {
