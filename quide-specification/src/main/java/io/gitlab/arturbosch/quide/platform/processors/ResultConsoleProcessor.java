2fc3c249e20a2523cd6e7ca02398b6351a0f1dff
abosch <arturbosch@gmx.de>
2017-03-03 23:21:59 +0100

    Introduce new console print processor

---
+++ quide-specification/src/main/java/io/gitlab/arturbosch/quide/platform/processors/ResultConsoleProcessor.java
@@ -0,0 +1,32 @@
+package io.gitlab.arturbosch.quide.platform.processors;
+
+import io.gitlab.arturbosch.quide.platform.ControlFlow;
+import io.gitlab.arturbosch.quide.platform.Processor;
+import io.gitlab.arturbosch.quide.platform.UserData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.stream.Collectors;
+
+/**
+ * @author Artur Bosch
+ */
+public class ResultConsoleProcessor implements Processor {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ResultConsoleProcessor.class.getSimpleName());
+
+	@Override
+	public <U extends UserData> void execute(U u) {
+		u.currentContainer().ifPresent(container -> {
+			String result = container.alive().stream()
+					.map(Object::toString)
+					.collect(Collectors.joining("\n"));
+			LOGGER.info("Analysis result: \n\n" + result);
+		});
+	}
+
+	@Override
+	public ControlFlow.InjectionPoint injectionPoint() {
+		return ControlFlow.InjectionPoint.AfterAnalysis;
+	}
+}
