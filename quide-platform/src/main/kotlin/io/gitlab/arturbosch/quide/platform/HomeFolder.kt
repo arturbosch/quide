b01d249fd54c36630367c126ee8d946cfb468110
abosch <arturbosch@gmx.de>
2017-03-05 23:49:04 +0100

    Support quide properties from within quideDirectory/specification project

--- quide-platform/src/main/kotlin/io/gitlab/arturbosch/quide/platform/HomeFolder.kt
+++ quide-platform/src/main/kotlin/io/gitlab/arturbosch/quide/platform/HomeFolder.kt
@@ -1,28 +1,8 @@
 package io.gitlab.arturbosch.quide.platform
 
-import java.nio.file.Files
-import java.nio.file.Path
-import java.util.Properties
-
 /**
  * @author Artur Bosch
  */
-object HomeFolder : QuideDirectory {
-	val propertiesPath: Path = home().resolve("quide.properties")
-}
+object HomeFolder : QuideDirectory.DefaultQuideDirectory()
 
-object QuideProperties {
-	private val properties = Properties().apply {
-		val propertiesPath = HomeFolder.propertiesPath
-		if (Files.notExists(propertiesPath)) Files.createFile(propertiesPath)
-		Files.newInputStream(propertiesPath).use {
-			load(it)
-		}
-	}
-
-	fun getOrNull(key: String): String? {
-		return properties.getProperty(key)
-	}
-}
-
-fun String.asProperty(): String? = QuideProperties.getOrNull(this)
+fun String.asProperty(): String? = HomeFolder.getProperty(this)
--- quide-specification/src/main/java/io/gitlab/arturbosch/quide/platform/QuideDirectory.java
+++ quide-specification/src/main/java/io/gitlab/arturbosch/quide/platform/QuideDirectory.java
@@ -1,10 +1,15 @@
 package io.gitlab.arturbosch.quide.platform;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 /**
  * @author Artur Bosch
@@ -36,5 +41,38 @@ public interface QuideDirectory {
 
 	default Path configurationsDir() {
 		return checkDir(home().resolve("configurations"));
+	}
+
+	default String getProperty(String key) {
+		return getProperties().getProperty(key);
+	}
+
+	Properties getProperties();
+
+	class DefaultQuideDirectory implements QuideDirectory {
+
+		private static final Logger LOGGER = LoggerFactory.getLogger(QuideDirectory.class.getSimpleName());
+
+		private Properties properties = new Properties();
+
+		{
+			Path propertiesPath = home().resolve("quide.properties");
+			try {
+				if (Files.notExists(propertiesPath)) {
+					Files.createFile(propertiesPath);
+				}
+				try (InputStream is = Files.newInputStream(propertiesPath)) {
+					properties.load(is);
+				}
+				LOGGER.info("Properties loaded");
+			} catch (IOException e) {
+				LOGGER.error("Error loading quide properties", e);
+			}
+		}
+
+		@Override
+		public Properties getProperties() {
+			return properties;
+		}
 	}
 }
