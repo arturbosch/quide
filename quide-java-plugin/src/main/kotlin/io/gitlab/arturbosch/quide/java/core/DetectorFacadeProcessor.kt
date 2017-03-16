adafe8196630a3e51e698a2b30c1a6d7d9fcd9a2
abosch <arturbosch@gmx.de>
2017-03-16 20:41:58 +0100

    Extract filter configurations to properties - #7

--- quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/core/DetectorFacadeProcessor.kt
+++ quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/core/DetectorFacadeProcessor.kt
@@ -5,6 +5,7 @@ import io.gitlab.arturbosch.quide.java.JavaPluginData
 import io.gitlab.arturbosch.quide.java.UPDATABLE_FACADE
 import io.gitlab.arturbosch.quide.platform.ControlFlow
 import io.gitlab.arturbosch.quide.platform.Processor
+import io.gitlab.arturbosch.quide.platform.QuideDirectory
 import io.gitlab.arturbosch.quide.platform.UserData
 import io.gitlab.arturbosch.smartsmells.api.DetectorFacade
 import io.gitlab.arturbosch.smartsmells.api.UpdatableDetectorFacade
@@ -19,20 +20,27 @@ class DetectorFacadeProcessor : Processor {
 	override fun <U : UserData> execute(data: U) {
 		val pluginData = data as JavaPluginData
 		val configPath = data.quideDirectory().configurationsDir().resolve("smartsmells.yml")
+		val filters = loadFiltersFromProperties(data.quideDirectory())
 		val facade = if (Files.exists(configPath)) {
 			DetectorFacade.builder()
-					.withFilters(".*/test/.*,.*/resources/.*")
+					.withFilters(filters)
 					.fromConfig(DetectorConfig.load(configPath))
 					.build()
 		} else {
 			DetectorFacade.builder()
-					.withFilters(".*/test/.*,.*/resources/.*")
+					.withFilters(filters)
 					.fullStackFacade()
 		}
 		if (pluginData.isEvolutionaryAnalysis()) {
 			data.put(UPDATABLE_FACADE, UpdatableDetectorFacade(facade))
 		}
 		data.put(FACADE, facade)
+	}
+
+	private fun loadFiltersFromProperties(quideDirectory: QuideDirectory): List<String> {
+		val globalFilters = quideDirectory.getProperty("path.filters.global").trim()
+		val javaFilters = quideDirectory.getProperty("path.filters.java").trim()
+		return globalFilters.split(',').plus(javaFilters.split(',')).filterNot(String::isNullOrBlank)
 	}
 
 	override fun injectionPoint(): ControlFlow.InjectionPoint {
