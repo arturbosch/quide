ca1d98348424dd2c4be12ac240638a957636444c
abosch <arturbosch@gmx.de>
2017-03-17 21:18:18 +0100

    Clean up version crawler

--- quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/core/VersionCrawler.kt
+++ quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/core/VersionCrawler.kt
@@ -12,28 +12,29 @@ import java.nio.file.Path
 /**
  * @author Artur Bosch
  */
-class VersionCrawler(val projectPath: Path, val versionable: Versionable,
+class VersionCrawler(private val projectPath: Path,
+					 private val versionable: Versionable,
 					 private val facade: UpdatableDetectorFacade) {
 
 	private val logger: Logger = LoggerFactory.getLogger(javaClass.simpleName)
 
 	fun run(): SmellResult {
-		return generateNewCompilationUnits(versionable).apply {
+		return generateNewCompilationUnits().apply {
 			logger.info("Successful executed detectors with ${smellSets.map { "${it.key} - ${it.value.size} " }}")
 		}
 	}
 
-	private fun generateNewCompilationUnits(current: Versionable): SmellResult {
+	private fun generateNewCompilationUnits(): SmellResult {
 
-		val sequence = current.fileChanges().asSequence()
+		val sequence = versionable.fileChanges().asSequence()
 		val additions = sequence.filter { it.isOfType(FileChange.Type.ADDITION) }
 				.map { it.newFile() }.map { projectPath.resolve(it.path()) to it.content() }
-				.filter { it.first.toString().endsWith(".java") }
+				.filter { it.first.toString().endsWith(JAVA) }
 				.filter { !it.second.isNullOrEmpty() }
 				.toMap()
 		val modifications = sequence.filter { it.isOfType(FileChange.Type.MODIFICATION) }
 				.map { it.newFile() }.map { projectPath.resolve(it.path()) to it.content() }
-				.filter { it.first.toString().endsWith(".java") }
+				.filter { it.first.toString().endsWith(JAVA) }
 				.filter { !it.second.isNullOrEmpty() }
 				.toMap()
 		val relocations = sequence.filter { it.isOfType(FileChange.Type.RELOCATION) }
@@ -41,16 +42,15 @@ class VersionCrawler(val projectPath: Path, val versionable: Versionable,
 					projectPath.resolve(change.oldFile().path()) to
 							Pair(projectPath.resolve(change.newFile().path()), change.newFile().content())
 				}
-				.filter { it.first.toString().endsWith(".java") }
-				.filter { it.second.a.toString().endsWith(".java") }
+				.filter { it.first.toString().endsWith(JAVA) }
+				.filter { it.second.a.toString().endsWith(JAVA) }
 				.filter { !it.second.b.isNullOrEmpty() }
 				.toMap()
 		val deletions = sequence.filter { it.isOfType(FileChange.Type.REMOVAL) }
 				.map { it.oldFile() }
 				.map { projectPath.resolve(it.path()) }
-				.filter { it.toString().endsWith(".java") }
+				.filter { it.toString().endsWith(JAVA) }
 				.toList()
-		logger.info("Parsed all changes...")
 		val size = additions.size + modifications.size + relocations.size
 		logger.info("Running detectors with $size compilation info's.")
 		facade.remove(deletions)
@@ -61,3 +61,5 @@ class VersionCrawler(val projectPath: Path, val versionable: Versionable,
 	}
 
 }
+
+private val JAVA = ".java"
