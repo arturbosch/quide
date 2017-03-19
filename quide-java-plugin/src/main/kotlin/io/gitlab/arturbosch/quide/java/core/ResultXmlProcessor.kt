5ab9f2e28222cbb26f0c9011475a3acac812934c
abosch <arturbosch@gmx.de>
2017-03-19 11:06:12 +0100

    Change java xml result should only depend on output.java.xml property - #23

--- quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/core/ResultXmlProcessor.kt
+++ quide-java-plugin/src/main/kotlin/io/gitlab/arturbosch/quide/java/core/ResultXmlProcessor.kt
@@ -4,7 +4,6 @@ import io.gitlab.arturbosch.quide.java.OUTPUT_JAVA_XML
 import io.gitlab.arturbosch.quide.java.safeContainer
 import io.gitlab.arturbosch.quide.platform.ControlFlow
 import io.gitlab.arturbosch.quide.platform.Processor
-import io.gitlab.arturbosch.quide.platform.QuideConstants
 import io.gitlab.arturbosch.quide.platform.UserData
 import io.gitlab.arturbosch.smartsmells.api.SmellResult
 import io.gitlab.arturbosch.smartsmells.out.XMLWriter
@@ -24,18 +23,10 @@ class ResultXmlProcessor : Processor {
 	private fun UserData.isReportWanted() = quideDirectory().getProperty(OUTPUT_JAVA_XML).toBoolean()
 
 	private fun <U : UserData> generateReport(data: U, smellResult: SmellResult) {
-		if (skipResultFile(data)) return
-
 		val outputPath = data.outputPath().orElse(data.projectPath())
 		val xml = XMLWriter.toXml(smellResult)
 		Files.write(outputPath.resolve("smartsmells_${LocalDateTime.now()}.xml"), xml.toByteArray())
 	}
-
-	private fun <U : UserData> skipResultFile(data: U): Boolean {
-		val property = data.quideDirectory().getProperty(QuideConstants.OUTPUT_FILE)
-		return !property.toBoolean()
-	}
-
 
 	override fun injectionPoint(): ControlFlow.InjectionPoint {
 		return ControlFlow.InjectionPoint.AfterAnalysis
