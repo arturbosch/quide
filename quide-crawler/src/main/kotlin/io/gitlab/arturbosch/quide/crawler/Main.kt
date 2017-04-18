4a4a82969ae93f8601085054bb1f3514201a5f07
abosch <arturbosch@gmx.de>
2017-04-18 11:53:03 +0200

    Remove gui options - #31

--- quide-crawler/src/main/kotlin/io/gitlab/arturbosch/quide/crawler/Main.kt
+++ quide-crawler/src/main/kotlin/io/gitlab/arturbosch/quide/crawler/Main.kt
@@ -1,36 +1,21 @@
 package io.gitlab.arturbosch.quide.crawler
 
-import io.gitlab.arturbosch.kutils.notNullOrFalse
 import io.gitlab.arturbosch.quide.crawler.cli.CLI
 import io.gitlab.arturbosch.quide.crawler.cli.parse
-import io.gitlab.arturbosch.quide.crawler.ui.RootView
-import javafx.application.Application
-import tornadofx.App
-import tornadofx.View
-import kotlin.reflect.KClass
 
 /**
  * @author Artur Bosch
  */
-class CrawlerApp : App() {
-	override val primaryView: KClass<out View> = RootView::class
-}
-
 fun main(args: Array<String>) {
 	with(args.parse()) {
 
 		val ioNull = input == null && output == null
 
-		if (ioNull && !withGUI.notNullOrFalse()) {
+		if (ioNull) {
 			println(usage)
 			return
 		}
 
-		if (withGUI.notNullOrFalse()) {
-			Application.launch(CrawlerApp::class.java, *args)
-		} else {
-			CLI.run(this)
-		}
-
+		CLI.run(this)
 	}
 }
--- quide-crawler/src/main/kotlin/io/gitlab/arturbosch/quide/crawler/cli/Args.kt
+++ quide-crawler/src/main/kotlin/io/gitlab/arturbosch/quide/crawler/cli/Args.kt
@@ -12,16 +12,15 @@ import java.nio.file.Path
  */
 object Args {
 	@Parameter(names = arrayOf("--input", "-i"),
+			required = true,
 			description = "The input git list in a file.",
 			converter = ExistingPathConverter::class)
 	var input: Path? = null
 	@Parameter(names = arrayOf("--output", "-o"),
+			required = true,
 			description = "The output report folder path.",
 			converter = DirectoryPathConverter::class)
 	var output: Path? = null
-	@Parameter(names = arrayOf("--gui"), description = "Use graphical interface?")
-	var withGUI: Boolean? = null
-
 	var usage: String = ""
 }
 
