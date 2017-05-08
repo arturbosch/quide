8764ae5c37020588894af49263dab69da8d02205
abosch <arturbosch@gmx.de>
2017-05-08 11:14:09 +0200

    Add script to compile and copy plugins to quide directory

---
+++ copyPlugins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./gradlew build fatjar
+cp $PWD/quide-java-plugin/build/libs/quide-java-plugin-1.0.0.M1.jar ~/.quide/plugins/quide-java-plugin-1.0.0.M1.jar
+cp $PWD/quide-detekt-plugin/build/libs/quide-detekt-plugin-1.0.0.M1.jar ~/.quide/plugins/quide-detekt-plugin-1.0.0.M1.jar
+cp $PWD/quide-groovy-plugin/build/libs/quide-groovy-plugin-1.0.0.M1.jar ~/.quide/plugins/quide-groovy-plugin-1.0.0.M1.jar
+echo "Copied plugins into .quide directory."
