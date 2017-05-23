2c7597581ca5a2057a06beaccffd277919b13f5d
abosch <arturbosch@gmx.de>
2017-05-23 10:48:51 +0200

    Add additional vcs constants concerning commits and time ranges

--- quide-specification/src/main/java/io/gitlab/arturbosch/quide/platform/QuideConstants.java
+++ quide-specification/src/main/java/io/gitlab/arturbosch/quide/platform/QuideConstants.java
@@ -3,31 +3,33 @@ package io.gitlab.arturbosch.quide.platform;
 /**
  * @author Artur Bosch
  */
-public final class QuideConstants {
-
-	private QuideConstants() {
-	}
+public interface QuideConstants {
 
 	// PLATFORM
-	public static final String DEBUG = "platform.debug";
-	public static final String PLATFORM_ADDITIONAL_PROPERTIES = "platform.additional.properties";
+	String DEBUG = "platform.debug";
+	String PLATFORM_ADDITIONAL_PROPERTIES = "platform.additional.properties";
 
-	public static final String PLATFORM_IGNORE_PLUGINS = "platform.ignore.plugins";
+	String PLATFORM_IGNORE_PLUGINS = "platform.ignore.plugins";
 
 	// INPUT
-	public static final String PATHS_FILTERS_GLOBAL = "input.paths.filters.global";
+	String PATHS_FILTERS_GLOBAL = "input.paths.filters.global";
 
 	// OUTPUT
-	public static final String OUTPUT_CONSOLE = "output.console";
-	public static final String OUTPUT_FILE = "output.file";
+	String OUTPUT_CONSOLE = "output.console";
+	String OUTPUT_FILE = "output.file";
 
 	// SHELL
-	public static final String SHELL_IGNORE_COMMANDS = "shell.ignore.commands";
+	String SHELL_IGNORE_COMMANDS = "shell.ignore.commands";
 
 	// VCS
-	public static final String VCS_BRANCH = "vcs.branch";
-	public static final String VCS_RELATIVE_PATH = "vcs.relative.path";
-	public static final String VCS_OUTPUT = "vcs.output";
-	public static final String VCS_OUTPUT_PER_VERSION = "vcs.output.perversion";
+	String VCS_BRANCH = "vcs.branch";
+	String VCS_RELATIVE_PATH = "vcs.relative.path";
+	String VCS_START_COMMIT = "vcs.start.commit.version";
+	String VCS_START_HASH = "vcs.start.commit.hash";
+	String VCS_RANGE_SINCE = "vcs.range.since";
+	String VCS_RANGE_UNTIL = "vcs.range.until";
+	String VCS_OUTPUT = "vcs.output";
+	String VCS_OUTPUT_PER_VERSION = "vcs.output.perversion";
+
 }
 
