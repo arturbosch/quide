2b1f82fdc5b2c5463b5ed5c87147eb2f451abbc7
Artur Bosch <arturbosch@gmx.de>
2017-07-01 16:37:11 +0200

    Reimplement quide directory in kotlin

---
+++ quide-api/src/main/kotlin/io.gitlab.arturbosch.quide.api/core/QuideDirectory.kt
@@ -0,0 +1,44 @@
+package io.gitlab.arturbosch.quide.api.core
+
+import java.io.File
+import java.io.IOException
+import java.util.HashMap
+
+/**
+ * @author Artur Bosch
+ */
+abstract class QuideDirectory(protected val properties: MutableMap<String, String> = HashMap()) {
+
+	companion object {
+		const val USER_HOME = "user.home"
+		const val QUIDE_DIR_NAME = ".quide"
+		const val PLUGINS_DIR_NAME = "plugins"
+		const val CONFIGURATIONS_DIR_NAME = "configurations"
+	}
+
+	val home: File = checkDir(File(System.getProperty(USER_HOME), QUIDE_DIR_NAME))
+	val pluginsDir: File = checkDir(resolve(PLUGINS_DIR_NAME))
+	val configurationsDir: File = checkDir(resolve(CONFIGURATIONS_DIR_NAME))
+
+	fun resolve(subPath: String): File {
+		return checkDir(home.resolve(subPath))
+	}
+
+	fun property(key: String): String? {
+		return properties[key]
+	}
+
+	fun propertyOrDefault(key: String, defaultValue: String): String {
+		return properties[key] ?: defaultValue
+	}
+
+	private fun checkDir(path: File): File {
+		if (!path.exists()) {
+			if (!path.mkdirs()) {
+				throw IOException("Error creating directories for " + path)
+			}
+		}
+		return path
+	}
+
+}
---
+++ quide-api/src/test/kotlin/io/gitlab/arturbosch/quide/api/core/QuideDirectoryTest.kt
@@ -0,0 +1,28 @@
+package io.gitlab.arturbosch.quide.api.core
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+/**
+ * @author Artur Bosch
+ */
+internal class QuideDirectoryTest : Spek({
+
+	describe("navigation inside the quide directory") {
+
+		val quide = object : QuideDirectory() {}
+
+		it("provides core paths inside quide") {
+			assertThat(quide.home.exists()).isTrue()
+			assertThat(quide.pluginsDir.exists()).isTrue()
+			assertThat(quide.configurationsDir.exists()).isTrue()
+		}
+
+		it("allows to retrieve quide.properties") {
+			assertThat(quide.property("test.test.test")).isNull()
+			assertThat(quide.propertyOrDefault("test.test.test", "Test")).isEqualTo("Test")
+		}
+	}
+})
