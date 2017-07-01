dded3b52db634df587efd2fee4b743b5eebe010f
Artur Bosch <arturbosch@gmx.de>
2017-07-01 22:53:04 +0200

    Remove operator modifier for get method on StorageAware as type inference will fail
    Users have to be explicit with the return type.

--- quide-api/src/main/kotlin/io.gitlab.arturbosch.quide.api/core/StorageAware.kt
+++ quide-api/src/main/kotlin/io.gitlab.arturbosch.quide.api/core/StorageAware.kt
@@ -4,6 +4,6 @@ package io.gitlab.arturbosch.quide.api.core
  * @author Artur Bosch
  */
 interface StorageAware {
-	operator fun <T> get(key: String): T?
+	fun <T> get(key: String): T?
 	fun <T : Any> put(key: String, value: T)
 }
