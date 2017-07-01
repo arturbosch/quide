0c1c4ddfcef13db13be73fad02597ad8424f6660
Artur Bosch <arturbosch@gmx.de>
2017-07-01 16:43:10 +0200

    Reimplement storage functionality in kotlin

---
+++ quide-api/src/main/kotlin/io.gitlab.arturbosch.quide.api/core/VersionAware.kt
@@ -0,0 +1,25 @@
+package io.gitlab.arturbosch.quide.api.core
+
+import io.gitlab.arturbosch.quide.vcs.VersionProvider
+import io.gitlab.arturbosch.quide.vcs.Versionable
+
+/**
+ * @author Artur Bosch
+ */
+interface VersionAware : StorageAware {
+
+	companion object {
+		const val LAST_CONTAINER = "lastContainer"
+		const val CURRENT_CONTAINER = "currentContainer"
+		const val LAST_VERSION = "lastVersion"
+		const val CURRENT_VERSION = "currentVersion"
+		const val VERSION_PROVIDER = "versionProviderRegistered"
+	}
+
+	val lastContainer get(): Versionable? = get(LAST_CONTAINER)
+	val currentContainer get(): Versionable? = get(CURRENT_CONTAINER)
+	val lastVersion get(): Versionable? = get(LAST_VERSION)
+	val currentVersion get(): Versionable? = get(CURRENT_VERSION)
+	val versionProvider get(): VersionProvider? = get(VERSION_PROVIDER)
+	val isEvolutionaryAnalysis get(): Boolean = versionProvider != null
+}
