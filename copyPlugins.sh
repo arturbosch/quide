7058a3a1e632cc4a881b3d87485cb8d575283b15
Artur Bosch <arturbosch@gmx.de>
2017-07-08 14:23:05 +0200

    Update to M3 and update build scripts

--- copyPlugins.sh
+++ copyPlugins.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 ./gradlew build fatjar
-cp $PWD/quide-java-plugin/build/libs/quide-java-plugin-1.0.0.M2.jar ~/.quide/plugins/quide-java-plugin-1.0.0.M2.jar
-cp $PWD/quide-detekt-plugin/build/libs/quide-detekt-plugin-1.0.0.M2.jar ~/.quide/plugins/quide-detekt-plugin-1.0.0.M2.jar
-cp $PWD/quide-groovy-plugin/build/libs/quide-groovy-plugin-1.0.0.M2.jar ~/.quide/plugins/quide-groovy-plugin-1.0.0.M2.jar
-echo "Copied plugins into .quide directory."
+find $PWD/quide-*-plugin/build/libs/ -type f | while read file ; do cp ${file} ~/.quide/plugins/ ; done
+echo "Copied plugins into tinbo directory."
